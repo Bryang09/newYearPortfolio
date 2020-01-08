a44d685dd58ef2b521f94fe781eb668b16212726
Bryan Gonzalez <bryan.gonzalez1020.bg@gmail.com>
2020-01-07 18:17:58 -0600

    Scroll Header Box-shadow added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,25 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import Nav from "./Nav/Nav";
 
 import { Link } from "react-router-dom";
 
 import "./Header.scss";
 
-const Header = () => {
-  return (
-    <div className="header">
-      <div className="logoContainer">
-        <Link to="/">
-          <div className="logo" />
-        </Link>
-      </div>
-      <div className="nav">
-        <div className="navContainer">
-          <Nav />
+class Header extends Component {
+  state = {
+    optClass: "top"
+  };
+
+  componentDidMount = () => {
+    window.addEventListener("scroll", () => {
+      window.scrollY >= 1
+        ? this.setState({ optClass: "scroll" })
+        : this.setState({ optClass: "top" });
+    });
+  };
+
+  render() {
+    console.log(this.state.optClass);
+    console.log(window.scrollY);
+
+    const { optClass } = this.state;
+
+    return (
+      <div
+        className={`header`}
+        style={
+          optClass === "top"
+            ? { boxShadow: "none" }
+            : { boxShadow: "1px 7px 80px" }
+        }
+      >
+        <div className="logoContainer">
+          <Link to="/">
+            <div className="logo" />
+          </Link>
+        </div>
+        <div className="nav">
+          <div className="navContainer">
+            <Nav />
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Header;
