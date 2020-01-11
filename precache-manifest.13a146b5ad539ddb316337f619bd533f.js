7bd7af9c1faefe9506b2f96374219d1ca17c06a7
Bryan Gonzalez <bryan.gonzalez1020.bg@gmail.com>
2020-01-11 15:11:09 -0600

    Updates

---
+++ precache-manifest.13a146b5ad539ddb316337f619bd533f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "028c281e0f621003fd92f0a304d03411",
+    "url": "/index.html"
+  },
+  {
+    "revision": "076b0deb98e24eb14007",
+    "url": "/static/css/main.46a78b04.chunk.css"
+  },
+  {
+    "revision": "cf6d9dc3e1cdb8774303",
+    "url": "/static/js/2.e78d34cd.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.e78d34cd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "076b0deb98e24eb14007",
+    "url": "/static/js/main.798384e4.chunk.js"
+  },
+  {
+    "revision": "2c901efb4850ab72f2ed",
+    "url": "/static/js/runtime-main.f588c2d3.js"
+  }
+]);
